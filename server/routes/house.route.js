96b9ef33f14668a944d642921bfd6cb0f0d09054
ngirimana <chadrackngirimana@gmail.com>
2020-03-16 12:57:37 +0200

    add authentication to api

--- server/routes/house.route.js
+++ server/routes/house.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { addHouse, editHouse } from '../controllers/houseController';
+import { verifyAuth } from '../middlewares/authentication';
 
 const houseRouter = express.Router();
 
-houseRouter.post('/house', addHouse);
-houseRouter.patch('/house/:houseId', editHouse);
+houseRouter.post('/house', verifyAuth, addHouse);
+houseRouter.patch('/house/:houseId', verifyAuth, editHouse);
 
 export default houseRouter;
