4547c2768224217a859798f5bf38e433ae88c924
ngirimana <chadrackngirimana@gmail.com>
2020-03-16 13:04:00 +0200

    find all houses api

--- server/routes/house.route.js
+++ server/routes/house.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addHouse, editHouse } from '../controllers/houseController';
+import { addHouse, editHouse, findAllHouse } from '../controllers/houseController';
 import { verifyAuth } from '../middlewares/authentication';
 
 const houseRouter = express.Router();
 
 houseRouter.post('/house', verifyAuth, addHouse);
 houseRouter.patch('/house/:houseId', verifyAuth, editHouse);
+houseRouter.get('/houses/', findAllHouse);
 
 export default houseRouter;
