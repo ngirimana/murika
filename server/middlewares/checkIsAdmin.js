3798a8a72f09cd785a2545fd04ccdec0379fb8b5
ngirimana <chadrackngirimana@gmail.com>
2020-03-18 07:32:37 +0200

    add middleware to check if user is admin

---
+++ server/middlewares/checkIsAdmin.js
@@ -0,0 +1,26 @@
+import User from '../models/userModel';
+import { userIdFromToken, isAdminFromToken } from '../helpers/token';
+import { errorResponse } from '../helpers/response';
+
+export const verifyAuth = async (req, res, next) => {
+  const authToken = req.header('x-auth-token');
+  if (!authToken) {
+    return errorResponse(res, 400, "You haven't provide your token");
+  }
+  try {
+    const userId = userIdFromToken(authToken);
+    const adminUser = isAdminFromToken(authToken);
+    const user = await User.findOne({ _id: userId });
+    if (user.isAdmin !== adminUser) {
+      return errorResponse(
+        res,
+        401,
+        'You are not authorized to perform this task',
+      );
+    }
+
+    next();
+  } catch (error) {
+    return errorResponse(res, 400, error.message);
+  }
+};
