1230f58801aeca00da42e9e38e9ad19430c537cc
ngirimana <chadrackngirimana@gmail.com>
2020-05-12 13:02:19 +0200

    remove unused modules

--- server/routes/house.route.js
+++ server/routes/house.route.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import fs from 'fs';
 import {
   addHouse, editHouse, findAllHouse, findOneHouse, rentHouse, getRentedHouse,
   searchHouse, getAllRentedHouse, deleteOneHouse, HouseIRented, activateHouse,
