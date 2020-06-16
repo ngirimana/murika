b0cb513e74b9e728cc5d494017c19dd0a242f4f8
ngirimana <chadrackngirimana@gmail.com>
2020-06-16 15:44:20 +0200

    set cors-orgin

--- server/config/defaultConfig.js
+++ server/config/defaultConfig.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   env: process.env.NODE_ENV,
 };
 
--- server/index.js
+++ server/index.js
@@ -20,6 +20,11 @@ mongoose
   })
   .then(() => process.stdout.write('DB Connection succesfully\n'));
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
 app.use('/api/v1/uploads', express.static('uploads'));
